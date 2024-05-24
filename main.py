6db1bb86893619f543d0d5faacc15cc242f2579f
IxAntho <isabar735@icloud.com>
2024-05-23 23:21:23 -0400

    Feat/login-logout functionality and pasword has and salt security 1st attempt

--- main.py
+++ main.py
@@ -10,20 +10,23 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
 
 
-DB_PATH = os.path.join(os.path.dirname(__file__), "posts.db")
+DB_PATH = os.path.join(os.path.dirname(__file__), "users.db")
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_PATH}'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
 
 # CREATE TABLE IN DB
-class User(db.Model):
+class User(db.Model, UserMixin):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
@@ -38,6 +41,12 @@ with app.app_context():
         print("Database already exists.")
 
 
+# Retrieving user object
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -50,10 +59,11 @@ def register():
         name = request.form['name']
         email = request.form['email']
         password = request.form['password']
+        hash_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
 
         new_user = User(
             email=email,
-            password=password,
+            password=hash_password,
             name=name
         )
         db.session.add(new_user)
@@ -69,25 +79,45 @@ def register():
         return render_template('register.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('secrets'))
+        else:
+            return 'Invalid email or password'
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    name = request.args.get('username')
-    return render_template("secrets.html", username=name)
+    return render_template("secrets.html", user=current_user)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
-    pass
+    FILE = os.path.join(os.path.dirname(__file__), "static/files/")
+    filename = "cheat_sheet.pdf"
+    # Check if the requested file exists in the download directory
+    if os.path.exists(os.path.join(FILE, filename)):
+        # Serve the requested file for download
+        return send_from_directory(FILE, filename, as_attachment=True)
+    else:
+        # Handle the case where the file doesn't exist
+        return "File not found", 404
 
 
 if __name__ == "__main__":
